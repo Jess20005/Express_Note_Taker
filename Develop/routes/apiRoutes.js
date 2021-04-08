7c3b1f0e9c90c757842c7be1d2bd5cff8f974ee5
Jessica <simply.me05@hotmail.com>
2021-04-08 16:53:27 -0400

    set up route folders, added uuid package

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,28 @@
+const notesData = require("../db/db.json");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (app) => {
+  app.get("/api/notes", (req, res) => {
+    res.json(notesData);
+    // console.log (notesData)
+  });
+
+  app.post("/api/notes", (req, res) => {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+
+    notesData.push(req.body);
+    fs.writeFile(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(notesData),
+      function (err) {
+        if (err) throw err;
+        res.json(notesData);
+      }
+    );
+    res.json(true);
+  });
+};
+//uuid package install with v4 add id note before line16
